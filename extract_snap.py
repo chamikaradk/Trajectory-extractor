fe0788033f84eb590182ad4197cb9f9ea0294af5
Chamikara Karunasena <114973175+chamikaradk@users.noreply.github.com>
2024-07-29 16:26:43 -0700

    Add files via upload
    First draft - file added

---
+++ extract_snap.py
@@ -0,0 +1,39 @@
+import MDAnalysis as mda
+from MDAnalysis.coordinates.XTC import XTCReader
+import numpy as np
+
+topology = 'F4_bulk.gro'
+trajectory = 'F4_bulk.xtc'
+interval_ps = 20
+
+print(f"Time interval selected : {interval_ps} ps")
+def write_to_file(universe, frame, filename, supercell):
+    atoms = universe.atoms
+    with open(filename, 'w') as f:
+        f.write(f"{len(atoms)}\n")
+        f.write(f"Frame {frame} coords box_coordinates {supercell} \n")
+        for atom in atoms:
+            pos = atom.position
+            f.write(f"{atom.name} {pos[0]:.3f} {pos[1]:.3f} {pos[2]:.3f} \n") 
+
+def extract_coords(topology, trajectory, interval_ps):
+    universe = mda.Universe(topology,trajectory)
+    dt = universe.trajectory.dt
+    interval_frames = int(interval_ps/dt)
+    box_coords = []
+    for ts in universe.trajectory[::interval_frames]:
+        box_coords.append(ts.dimensions)
+        frame, time = ts.frame, ts.time
+        supercell = ts.dimensions.copy()
+        supercell[:3]=supercell[:3]/10
+        filename = f"frame_{frame}.xyz"
+        write_to_file(universe, frame, filename, supercell)
+        print(f"File written: {filename}")
+    box_coords = np.array(box_coords)
+    return box_coords
+
+#box coordinates are stored in this array
+box_coordinates = extract_coords(topology, trajectory, interval_ps)
+box_coordinates[:,:3] = box_coordinates[:,:3]/10
+
+print(box_coordinates)
